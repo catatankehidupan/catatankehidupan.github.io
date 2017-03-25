fae03267a1912341286b69492816f8814b426c01
mzaini30 <muhzaini30@gmail.com>
2017-03-25 08:06:09 +0800

    oke

---
+++ app.py
@@ -0,0 +1,55 @@
+import glob, re
+
+lokasi = "/sdcard/Download/blog/"
+home = open(lokasi+"index.html", "w")
+header1 = open(lokasi+"_komponen/header1.txt").read()
+header2 = open(lokasi+"_komponen/header2.txt").read()
+header3 = open(lokasi+"_komponen/header3.txt").read()
+footer = open(lokasi+"_komponen/footer.txt").read()
+data = open(lokasi+"html/bin/data.js", "w")
+dataawal = open(lokasi+"_komponen/data awal.txt").read()
+dataakhir = open(lokasi+"_komponen/data akhir.txt").read()
+data1 = open(lokasi+"_komponen/data1.txt").read()
+data2 = open(lokasi+"_komponen/data2.txt").read()
+data3 = open(lokasi+"_komponen/data3.txt").read()
+
+postingan = glob.glob(lokasi+"post/*.md")
+print postingan
+fpostingan = postingan[:]
+target = postingan[:]
+judul = postingan[:]
+link = postingan[:]
+for n, x in enumerate(target):
+    target[n] = re.sub(r"/post/", r"/html/", target[n])
+    target[n] = target[n][:-3] + ".html"
+print target
+for n, x in enumerate(judul):
+    judul[n] = re.sub(r""+lokasi+"post/", r"", judul[n])
+    judul[n] = re.sub(r".md", r"", judul[n])
+    judul[n] = judul[n].title()
+print judul
+for n, x in enumerate(postingan):
+    fpostingan[n] = []
+    fpostingan[n].append(header1)
+    fpostingan[n].append(judul[n])
+    fpostingan[n].append(header2)
+    fpostingan[n].append(judul[n])
+    fpostingan[n].append(header3)
+    fpostingan[n].append(open(x).read())
+    fpostingan[n].append(footer)
+    fpostingan[n] = "".join(str(x) for x in fpostingan[n])
+    open(target[n], "w").write(fpostingan[n])
+for n, x in enumerate(link):
+    link[n] = re.sub(r""+lokasi+"post/", r"", link[n])
+    link[n] = re.sub(r".md", r".html", link[n])
+print link
+home.write("<script>location.href='html/"+link[0]+"'</script>")
+inputdata = dataawal
+for n, x in enumerate(judul):
+    inputdata += data1
+    inputdata += x
+    inputdata += data2
+    inputdata += link[n]
+    inputdata += data3
+inputdata += dataakhir
+data.write(inputdata)
